063ad257826ec238c813185010af28649f80c347
Karim Gonzalez <karim.gonzalez@bosonit.com>
2023-01-26 13:57:19 +0100

    Ejercicio 10

---
+++ ejercicio_10.js
@@ -0,0 +1,28 @@
+// Crea una función que elimine las etiquetas html o xml de un string.
+// La función debe tener un string como único parámetro.
+
+// Ejemplo de uso de la función:
+// const result = removeHTMLTags('<div><span>lorem</span> <strong>ipsum</strong></div>');
+// console.log(result); // lorem ipsum
+
+const ejemplo = '<div><span>lorem</span> <strong>ipsum</strong></div>';
+
+const removeHtml = (string) =>{
+  let array = string.split("")
+  let aux = 0;
+  let result = []
+  for(let i=0; i < array.length; i++){
+    if(array[i] === '<'){
+      aux += 1;
+    }
+    if(array[i] === '>'){
+      aux -= 1;
+    }
+    if(aux === 0 && array[i] !== '>'){
+      result.push(array[i])
+    }
+  }
+  console.log(result.join(""))
+}
+
+removeHtml(ejemplo)
